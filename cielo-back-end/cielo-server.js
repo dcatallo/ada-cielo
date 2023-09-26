ab4db34e86ea1e964d3e49ceb46fa489b3fbbd3e
dcatallo <diego.catallo5@gmail.com>
2023-09-26 19:31:11 -0300

    first commit

---
+++ cielo-back-end/cielo-server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const salesData = require('./data/salesData.json');
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define a sample API route
+app.get('/api/data', (req, res) => {
+  const data = salesData;
+  res.json(data);
+});
+
+const port = 3001; // You can choose any available port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
